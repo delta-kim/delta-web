e697d9d88c00b21eae25a7a5df685e4ac27c1ded
haida333 <haida333@outlook.com>
2024-10-14 17:08:22 +0800

    Update index.js

--- src/declarations/delta/index.js
+++ src/declarations/delta/index.js
@@ -23,7 +23,7 @@ export const createActor = async (canisterId, options = {}) => {
 
   // Fetch root key for certificate validation during development
   //if (process.env.DFX_NETWORK !== "ic") {
-  if(options.agentOptions.host){
+  if (options.agentOptions.host.startsWith("http://")) {
     // agent.fetchRootKey().catch((err) => {
     //   console.warn(
     //     "Unable to fetch root key. Check to ensure that your local replica is running"
