33cad9321cfccbb7a6afb935efe0a1aad0e665a8
haida333 <haida333@outlook.com>
2025-03-08 20:45:23 +0800

    Update store.ts

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -5,4 +5,4 @@ export const principal: Writable<string> = writable("");
 export const delta: Writable<any | null> = writable(null);
 export const roadMap: Writable<any | null> = writable(null);
 export const ic_host: Readable<string> = readable("https://ic0.app");
-export const roadMapCanisterId: Readable<string> = readable("mgul3-nqaaa-aaaam-adtiq-ca");
+export const roadMapCanisterId: Readable<string> = readable("mgul3-nqaaa-aaaam-adtiq-cai");
