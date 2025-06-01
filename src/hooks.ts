1c8c00526f7f6316474e2ae9e7193f24aee251ba
haida333 <haida333@outlook.com>
2025-06-01 18:49:16 +0800

    Update hooks.ts

--- src/hooks.ts
+++ src/hooks.ts
@@ -15,42 +15,42 @@ export const handle: Handle = async ({ event, resolve }) => {
             headers: { 'Content-Type': 'text/plain' }
         });
     }
-    if (event.url.pathname.startsWith("/canister_status/")) {
-        const deltaAgent = createDelta("ojpsk-siaaa-aaaam-adtea-cai", { agentOptions: { host: "https://ic0.app" } });
-        function replacer(key, value) {
-            if (typeof value === 'bigint') return Number(value);
-            return value;
-        }
-        const actor_delta = await deltaAgent;
-        let canisterId = event.url.pathname.split("/")[2];
-        try {
-            const kv = event.platform?.env?.KV;
-            const key = `CanisterStatus:${canisterId}`;
-            let json = await kv.get(key, 'json');
-            const now = Math.round(Date.now() / 1000);
-            if (json == null || json.update + 5 < now) {
-                if (json != null) { // 标记时间，防止重复get
-                    json.update = now;
-                    kv.put(key, JSON.stringify(json));
-                }
-                let status = await actor_delta.getCanisterStatus(canisterId);
-                for (const i in status) {
-                    status[i][1] = Number(status[i][1]);
-                }
-                json = { status, update: now };
-                let json_str = JSON.stringify(json);
-                await kv.put(key, json_str);
-                return new Response(JSON.stringify(json.status), {
-                    headers: { "Content-Type": "application/json" },
-                });
-            }
-            return new Response(JSON.stringify(json.status), {
-                headers: { "Content-Type": "application/json" },
-            });
-        } catch (error) {
-            return new Response(error.message);
-        }
-    }
+    // if (event.url.pathname.startsWith("/canister_status/")) {
+    //     const deltaAgent = createDelta("ojpsk-siaaa-aaaam-adtea-cai", { agentOptions: { host: "https://ic0.app" } });
+    //     function replacer(key, value) {
+    //         if (typeof value === 'bigint') return Number(value);
+    //         return value;
+    //     }
+    //     const actor_delta = await deltaAgent;
+    //     let canisterId = event.url.pathname.split("/")[2];
+    //     try {
+    //         const kv = event.platform?.env?.KV;
+    //         const key = `CanisterStatus:${canisterId}`;
+    //         let json = await kv.get(key, 'json');
+    //         const now = Math.round(Date.now() / 1000);
+    //         if (json == null || json.update + 5 < now) {
+    //             if (json != null) { // 标记时间，防止重复get
+    //                 json.update = now;
+    //                 kv.put(key, JSON.stringify(json));
+    //             }
+    //             let status = await actor_delta.getCanisterStatus(canisterId);
+    //             for (const i in status) {
+    //                 status[i][1] = Number(status[i][1]);
+    //             }
+    //             json = { status, update: now };
+    //             let json_str = JSON.stringify(json);
+    //             await kv.put(key, json_str);
+    //             return new Response(JSON.stringify(json.status), {
+    //                 headers: { "Content-Type": "application/json" },
+    //             });
+    //         }
+    //         return new Response(JSON.stringify(json.status), {
+    //             headers: { "Content-Type": "application/json" },
+    //         });
+    //     } catch (error) {
+    //         return new Response(error.message);
+    //     }
+    // }
     if (event.url.pathname == "/") {
         const negotiator = new Neg({
             headers: {
