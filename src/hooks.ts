9c6f34243d0211433ff8b60a533a9cae262b7da3
haida333 <haida333@outlook.com>
2025-06-01 18:42:19 +0800

    Update hooks.ts

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,10 +1,14 @@
+// hooks.server.ts
 import type { Handle, HandleFetch, RequestHandler } from '@sveltejs/kit';
 import { redirect } from '@sveltejs/kit';
 //var Negotiator = require('negotiator')
 import Neg from 'negotiator';
 import { initialLocale } from '$lib/i18n'; // Import to initialize. Important :)
 import { langs } from '$lib/i18n/langs';
+import { createActor as createDelta } from "./declarations/delta/index";
 let langKeys = Object.keys(langs);
+
+const deltaAgent = createDelta("ojpsk-siaaa-aaaam-adtea-cai", { agentOptions: { host: "https://ic0.app" } });
 
 export const handle: Handle = async ({ event, resolve }) => {
     if (event.url.pathname == '/google412d55fb8d43b8ef.html') {
@@ -12,24 +16,41 @@ export const handle: Handle = async ({ event, resolve }) => {
             headers: { 'Content-Type': 'text/plain' }
         });
     }
-    // if (event.url.pathname.startsWith("/canister_status/")) {
-    //     const kv = event.platform?.env?.KV;
-    //     if (!kv) throw new Error('KV not bound!');
-
-    //     // 1. 尝试从 KV 读取
-    //     let cachedData = await kv.get('data-1727092904161', 'json');
-
-    //     return new Response(JSON.stringify(cachedData));
-
-
-    //     // // 2. 无缓存时生成数据
-    //     // const newData = { timestamp: Date.now(), value: 'Data from KV!' };
-    //     // await kv.put('cached-data', JSON.stringify(newData), {
-    //     //     expirationTtl: 3600, // 1 小时后过期
-    //     // });
-
-    //     //return new Response(JSON.stringify(newData));
-    // }
+    if (event.url.pathname.startsWith("/canister_status/")) {
+        function replacer(key, value) {
+            if (typeof value === 'bigint') return Number(value);
+            return value;
+        }
+        const actor_delta = await deltaAgent;
+        let canisterId = event.url.pathname.split("/")[2];
+        try {
+            const kv = event.platform?.env?.KV;
+            const key = `CanisterStatus:${canisterId}`;
+            let json = await kv.get(key, 'json');
+            const now = Math.round(Date.now() / 1000);
+            if (json == null || json.update + 5 < now) {
+                if (json != null) { // 标记时间，防止重复get
+                    json.update = now;
+                    kv.put(key, JSON.stringify(json));
+                }
+                let status = await actor_delta.getCanisterStatus(canisterId);
+                for (const i in status) {
+                    status[i][1] = Number(status[i][1]);
+                }
+                json = { status, update: now };
+                let json_str = JSON.stringify(json);
+                await kv.put(key, json_str);
+                return new Response(JSON.stringify(json.status), {
+                    headers: { "Content-Type": "application/json" },
+                });
+            }
+            return new Response(JSON.stringify(json.status), {
+                headers: { "Content-Type": "application/json" },
+            });
+        } catch (error) {
+            return new Response(error.message);
+        }
+    }
     if (event.url.pathname == "/") {
         const negotiator = new Neg({
             headers: {
@@ -59,4 +80,4 @@ export const handle: Handle = async ({ event, resolve }) => {
     }
     await initialLocale(lang);
     return resolve(event);
-};
+}
