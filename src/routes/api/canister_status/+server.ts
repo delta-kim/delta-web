f81220a59e2b5cb2bb8a4e19d66137b462f2d190
haida333 <haida333@outlook.com>
2025-06-02 09:26:41 +0800

    Update +server.ts

--- src/routes/api/canister_status/+server.ts
+++ src/routes/api/canister_status/+server.ts
@@ -1,30 +1,16 @@
 // src/routes/api/user/+server.ts
 import type { RequestHandler } from '@sveltejs/kit';
-import { get } from 'svelte/store';
-import { ic_host } from "../../../lib/store";
-import { createActor as createDelta } from "../../../declarations/delta/index";
 export const GET: RequestHandler = async (event) => {
     let canisterId = event.url.searchParams.get("id");
+    if (canisterId == null) {
+        return new Response("Parameter exception");
+    }
     try {
         const kv = event.platform?.env?.KV;
         const key = `CanisterStatus:${canisterId}`;
         let json = await kv.get(key, 'json');
-        const now = Math.round(Date.now() / 1000);
-        if (json == null || json.update + 5 < now) {
-            if (json != null) { // 标记时间，防止重复get
-                json.update = now;
-                kv.put(key, JSON.stringify(json));
-            }
-
-            const deltaAgent = await createDelta("ojpsk-siaaa-aaaam-adtea-cai", { agentOptions: { host: get(ic_host) } });
-            let status = await deltaAgent.getCanisterStatus(canisterId);
-            for (const i in status) {
-                status[i][1] = Number(status[i][1]);
-            }
-            json = { status, update: now };
-            let json_str = JSON.stringify(json);
-            await kv.put(key, json_str);
-            return new Response(JSON.stringify(json.status), {
+        if (json == null) {
+            return new Response(JSON.stringify([["memory_size", 1000]]), {
                 headers: { "Content-Type": "application/json" },
             });
         }
