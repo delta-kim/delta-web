a12fce592e9a45e900a2b1eb30819c557f616fd7
haida333 <haida333@outlook.com>
2026-02-07 21:45:29 +0800

    DSMSTerminal download link 1.0.8

--- src/routes/[lang]/DSMSAccessTerminal/+page.svelte
+++ src/routes/[lang]/DSMSAccessTerminal/+page.svelte
@@ -111,7 +111,7 @@
             target="_blank"
             href={lang == "zh-CN"
               ? "https://wwrm.lanzoue.com/delta-dsms-terminal"
-              : "https://github.com/delta-kim/dsms_verifier_terminal/releases/download/1.0.7/dsms_terminal_1.0.7.apk"}
+              : "https://github.com/delta-kim/dsms_verifier_terminal/releases/download/1.0.8/dsms_terminal_1.0.8.apk"}
           >
             <img
               class="rounded-md w-[185px] object-contain"
@@ -123,7 +123,7 @@
             <a
               style="text-decoration: underline;"
               href="https://github.com/delta-kim/dsms_verifier_terminal/releases"
-              target="_blank">Version:1.0.7 Latest:2025-09-26</a
+              target="_blank">Version:1.0.8 Latest:2026-02-07</a
             >
           </div>
         </figure>
